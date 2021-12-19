833127f99dca43f1443ec965edeaabd0d0560f5e
Berat Yeşbek <beratyesbek@gmail.com>
2021-12-19 19:20:15 +0300

    Add project files.

---
+++ DataStructure/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using DataStructure.SingleLinkedList;
+
+namespace DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AddFirstToSinglyLinkedList();
+            //AddLastToSinglyLinkedList();
+            AddAfterToSinglyLinkedList();
+        }
+
+        private static void AddFirstToSinglyLinkedList()
+        {
+            var linkedList = new SinglyLinkedList<int>();
+            linkedList.AddFirst(1);
+            linkedList.AddFirst(2);
+            linkedList.AddFirst(3);
+            Console.ReadKey();
+        }
+
+        private static void AddLastToSinglyLinkedList()
+        {
+            var linkedList = new SinglyLinkedList<int>();
+            linkedList.AddLast(5);
+            linkedList.AddLast(4);
+            linkedList.AddLast(3);
+            Console.ReadKey();
+        }
+
+        private static void AddAfterToSinglyLinkedList()
+        {
+            var linkedList = new SinglyLinkedList<int>();
+            linkedList.AddFirst(1);
+            linkedList.AddFirst(2);
+            linkedList.AddFirst(3);
+            linkedList.AddFirst(4);
+            linkedList.AddAfter(linkedList.Head.Next, 31);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ DataStructure/SingleLinkedList/SinglyLinkedList.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.SingleLinkedList
+{
+    public class SinglyLinkedList<T>
+    {
+        public SinglyLinkedListNode<T> Head { get; set; }
+
+        public void AddFirst(T value)
+        {
+            var newNode = new SinglyLinkedListNode<T>(value);
+            newNode.Next = Head;
+            Head = newNode;
+        }
+
+        public void AddLast(T value)
+        {
+            var newNode = new SinglyLinkedListNode<T>(value);
+
+            if (Head == null)
+            {
+                Head = newNode;
+                return;
+            }
+
+            var current = Head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+
+            current.Next = newNode;
+        }
+
+        public void AddAfter(SinglyLinkedListNode<T> node, T value)
+        {
+            if (node == null)
+            {
+                throw new ArgumentException("Null reference");
+            }
+
+            if (Head == null)
+            {
+                AddFirst(value);
+                return;
+            }
+
+            var newNode = new SinglyLinkedListNode<T>(value);
+            var current = Head;
+            while (current != null)
+            {
+                if (current.Equals(node))
+                {
+                    newNode.Next = current.Next;
+                    current.Next = newNode;
+                    return;
+                }
+
+                current = current.Next;
+            }
+
+            throw new ArgumentException("The reference has not been found in this list");
+        }
+
+        public void AddBefore(SinglyLinkedListNode<T> node, T value)
+        {
+            if (node == null)
+            {
+                throw new ArgumentException("Null reference");
+            }
+
+            if (Head == null)
+            {
+                AddFirst(value);
+                return;
+            }
+
+            var newNode = new SinglyLinkedListNode<T>(value);
+            var current = Head;
+            while (current !=  null)
+            {
+                if (current.Equals(node))
+                {
+                    newNode.Next = current;
+                    current.Next = newNode;
+                    return;
+                }
+
+                current = current.Next;
+            }
+            throw new ArgumentException("The reference has not been found in this list");
+
+
+        }
+    }
+}
